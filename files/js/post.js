374af8d7934d6ac0d1b32bd853b3f928ae9ba8c6
Adnan-D-Luffy <abdullahal467bp@gmail.com>
2025-06-08 04:48:14 -0400

    added see more

--- files/js/post.js
+++ files/js/post.js
@@ -1,5 +1,5 @@
 const posts = document.querySelectorAll(".post-msge");
-
+const msgeP = [];
 
 
 posts.forEach(value=>{
@@ -14,19 +14,30 @@ posts.forEach(value=>{
     }
 })
 
+posts.forEach((value,i)=>{
+    value.setAttribute("data-msgeID",`${i}`)
+    
+})
 // what if the post is bigger than ? 300 words ? 
 //<span>See More</span> <span></span>
 posts.forEach(value => {
     let txt = value.innerText;
     const ptn = txt.length;
+    const id = value.dataset.msgeid;
+   
     if (Number(ptn)>300) {
+         const ID = Number(id);
+         let text ="";
+         msgeP.push({
+            ID ,
+            txt
+         });
          txt = txt.split("");
-         let text ;
          for (let i = 0; i < 250; i++) {
             text += txt[i];
          }
          value.innerHTML = `
-         ${text} <span class="See-More">... See More</span>
+         ${text} <span class="See-More" data-seeMore="${id}">... See More</span>
          `
     }
 });
@@ -34,6 +45,14 @@ posts.forEach(value => {
 const Seemore = document.querySelectorAll(".See-More");
 Seemore.forEach(value =>{
     value.addEventListener('click',()=>{
-        ///gonna do it letter ...InshAAllah
+        const id = value.dataset.seemore;
+        const index = msgeP.findIndex(data => data.ID === Number(id));
+        if (index !== -1) {
+           const text = msgeP[index].txt;
+           posts[id].innerHTML = text ;
+        }
+        else {
+            alert("Please enable js")
+        }
     })
 })
